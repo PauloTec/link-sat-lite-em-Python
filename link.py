9a81b3b8107328a709fb896de13adf01b68eb7e7
Diassilua Paulo Simão <diassilua.paulo.simao@outlook.com>
2021-09-03 20:36:49 +0100

    Cálculo do Ângulo de elevação e Azimute das estações terrenas

--- link.py
+++ link.py
@@ -1,37 +1,78 @@
 import math
-print('----------------------------------------------')
-print('     KUNDIAMA, TECNOLOGIA E INOVAÇÃO')
-print('')
-print('         CURSO DE TELECOMUNICAÇÕES')
-print('')
-print('         CÁLCULO DE LIGAÇÃO POR SATÉLITE ')
-print('')
-print('         Engº DIASSILUA PAULO SIMÃO')
-print('----------------------------------------------')
+
+print('|-------------------------------------------------------------|')
+print('|              Kundiama, Tecnologia e Inovação                |')
+print('|            Telecomunicações - Radiocomunicação              |')
+
+print('|                                                             |')
+print('|    Cálculo de Ligação Por Satélite entre Zona 1 e Zona 2  |')
+print('|                                                             |')
+
+print('| Elaborado por:                                              |')
+print('|            Engº Diassilua paulo Simao (10093)               |')
+print('|                                                             |')
+
+print('| Em memória: Professor Dr. Engº Alberto Amaral Lopes         |')
+print('|-------------------------------------------------------------|')
 
 #CONSTANTES DO SISTEMA
-
 RaioTerra = 6378.173 #Raio da terra em Km
 CentroMassa = 42158 #Centro de massa em Km
 
-#CALCULO DA DISTÂNCIA ENTRE A ESTAÇÃO TERRENA E O SATÉLITE
+#---------------------- INSERIR DADOS PARA O CÁLCULO
 
-latitudeEstacaoTerrena = int(input("Insere a latitude da estação terrena: "))
-print()
-longitudeEstacaoTerrena = int(input("Insere a longitude da estação terrena: "))
-print()
+#-- 1º LOCALIZAÇÃO DAS ESTAÇÕES
+nomeEstacaoA = input('Nome da localização da estaçao A - ')
+nomeEstacaoB = input('Nome da localização da estaçao B - ')
+
+#-- 1.1º Latitudes e longitudes das estações
+latitudeEstacaoTerrenaA = int(input("Insere a latitude da estação terrena 1: "))
+longitudeEstacaoTerrenaA = int(input("Insere a longitude da estação terrena 1: "))
+
+latitudeEstacaoTerrenaB = int(input("Insere a latitude da estação terrena 1: "))
+longitudeEstacaoTerrenaB = int(input("Insere a longitude da estação terrena 1: "))
+
+#-- 1.2º Latitudes e longitudes do satélite
 longitudeSatelite = int(input("Insere a longitude do satélite: "))
-print()
 
+#CALCULO DA DISTÂNCIA ENTRE A ESTAÇÃO TERRENA E O SATÉLITE
 aux1 = math.pow(RaioTerra,2) + math.pow(CentroMassa,2)
 aux2 = 2*RaioTerra*CentroMassa
-aux3 = math.cos(latitudeEstacaoTerrena)*math.cos(longitudeEstacaoTerrena-longitudeSatelite)
+aux3 = math.cos(latitudeEstacaoTerrenaA)*math.cos(longitudeEstacaoTerrenaA-longitudeSatelite)
+aux4 = math.cos(latitudeEstacaoTerrenaB)*math.cos(longitudeEstacaoTerrenaB-longitudeSatelite)
 
-distancia_sat_EstTerrena = math.sqrt(aux1 - (aux2*aux3))
+distancia_sat_EstTerrenaA = math.sqrt(aux1 - (aux2*aux3))
+distancia_sat_EstTerrenaB = math.sqrt(aux1 - (aux2*aux4))
+
+#CALCULO DO ÂNGULO DE ELEVAÇÃO DAS ESTAÇÕES TERRENAS
+#----Estação A
+auxiliar1 = CentroMassa/RaioTerra
+auxiliar11 = math.cos(latitudeEstacaoTerrenaA)*math.cos(longitudeEstacaoTerrenaA-longitudeSatelite)
+auxiliar12 = math.pow(auxiliar1,2)
+
+elevacaoA = (aux1*auxiliar11-1)/(math.sqrt(1+(auxiliar12))-(2*auxiliar1*auxiliar11))
+anguloElevacaoA = 90-math.acos(elevacaoA)
+#-----------------------
+#CALCULO DO AZIMUTE DAS ESTAÇÕES TERRENAS
+a = math.cos(longitudeEstacaoTerrenaA-longitudeSatelite)*math.sin(latitudeEstacaoTerrenaA)
+b = math.cos(latitudeEstacaoTerrenaA)*math.cos(longitudeSatelite-longitudeEstacaoTerrenaA)
+azim = a/b
+azimuteA = 180 - math.acos(azim)
+
+#-- 2º PARÂMETROS DO SISTEMA
+frequenciaUplink = input('Digite a Frequencia de uplink Fu =  ')
+frequenciaDownlink = input('Digite a Frequencia de Downlink  Fd =  ')
+print('')
+densidadeFluxoPorTransponder = input('Digite a Densidade de fluxo no Satélite p/ Sat Transponder =   ')
+fatorMeritoSatelite = input('Digite o Factor de Mérito Antena do Satélite, Uplink (relGTu) =  ')
+backOffEntrada = input('Digite Back off de entrada do Satélite (BOin) =  ')
+backOffSaida = input('Digite Back off de Saída do Satélite (BOout) = ')
+bitErrorRate = input('Digite Probabilidade ou Erro de Bit (BER)')
 
 print("---------------------------------------------------------------------------------")
-print("Latitude Estação terrena - ",latitudeEstacaoTerrena,"º")
-print("Longitude Estação terrena - ",longitudeEstacaoTerrena,"º")
+print("Latitude Estação terrena - ",latitudeEstacaoTerrenaA,"º")
+print("Longitude Estação terrena - ",longitudeEstacaoTerrenaA,"º")
 print("Longitude Do Satélite - ",longitudeSatelite,"º")
 print()
-print("Distância Entre Estação terrena e o Satélite - ", distancia_sat_EstTerrena,"km")
+print("Distância Entre Estação terrena A e o Satélite - ", distancia_sat_EstTerrenaA,"km")
+print("Distância Entre Estação terrena B e o Satélite - ", distancia_sat_EstTerrenaB,"km")
